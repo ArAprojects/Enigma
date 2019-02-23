e5501f4d5a4ebe491f73b7408bf2a0c11195d57f
Alek <alekaker@outlook.com>
2019-02-23 13:23:10 -0700

    adding enigma decryption test

--- test/engima_test.rb
+++ test/engima_test.rb
@@ -3,17 +3,27 @@ require './test/test_helper'
 
 class EnigmaTest < MiniTest::Test
 
-def test_it_exists
-  enigma = Enigma.new
-  assert_instance_of Enigma, enigma
-end
+  def test_it_exists
+    enigma = Enigma.new
+    assert_instance_of Enigma, enigma
+  end
 
-def test_enigma_can_encrypt
-  enigma = Enigma.new
-  expected = {
-    encryption: "keder ohulw",
-    key: "02715",
-   date: "040895"
- }
- assert_equal expected, enigma.decrypt("keder ohulw", "02715", "040895")
-end
+  def test_enigma_can_encrypt
+    enigma = Enigma.new
+    expected = {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    }
+    assert_equal expected, enigma.decrypt("keder ohulw", "02715", "040895")
+  end
+
+  def test_engigma_can_decrypt
+    enigma = Enigma.new
+    expected = {
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+  }
+  assert_equal expected, enigma.decrypt("keder ohulw", "02715", "040895")
+  end
