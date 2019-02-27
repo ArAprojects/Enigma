69da24cdd55bb207752e920c2e39e2c8ca7b9a46
Alek <alekaker@outlook.com>
2019-02-26 19:17:11 -0700

    adding tests to see if sums of offsets and keys are integers

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -8,4 +8,23 @@ class ShiftTest < MiniTest::Test
     assert_instance_of Integer, engima.shift_a
   end
 
+  def test_key_b_and_offset_b_sum_is_integer
+    enigma = Enigma.new
+    assert_instance_of Integer, engima.shift_b
+  end
+
+  def test_key_c_and_offset_c_sum_is_integer
+    enigma = Enigma.new
+    assert_instance_of Integer, engima.shift_c
+  end
+
+  def test_key_d_and_offset_d_sum_is_integer
+    enigma = Enigma.new
+    assert_instance_of Integer, engima.shift_d
+  end
+
+
+
+
+
 end
