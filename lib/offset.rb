88063a1e1fcba4dcb03d019fb3f4521cfe389f39
Alek <alekaker@outlook.com>
2019-02-26 18:56:23 -0700

    testing offset outputs are strings

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,7 +1,13 @@
 module Offset
 
-def todays_date_squared
-  todays_date.to_i**2
-end
+  def todays_date_squared
+    todays_date.to_i**2
+  end
+
+  def offset_a
+    todays_date_squared.to_s[-4]
+  end
+
+
 
 end
--- test/offset_test.rb
+++ test/offset_test.rb
@@ -14,4 +14,17 @@ class OffsetTest < MiniTest::Test
     assert_equal Date.today.strftime('%d%m%y').to_i**2, enigma.todays_date_squared
   end
 
+  def test_is_the_offset_a_string
+    enigma = Enigma.new
+    Date.today.strftime('%d%m%y')
+    assert_instance_of String, enigma.offset_a
+    assert_instance_of String, enigma.offset_b
+    assert_instance_of String, enigma.offset_c
+    assert_instance_of String, enigma.offset_d
+    assert_equal "7", enigma.offset_a
+    assert_equal "9", enigma.offset_b
+    assert_equal "6", enigma.offset_c
+    assert_equal "1", enigma.offset_d
+  end
+
 end
