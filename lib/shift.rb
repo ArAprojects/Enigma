e0a3a3d3f493508a5547766e8785829212e47d8e
Alek <alekaker@outlook.com>
2019-02-26 19:14:01 -0700

    creating shift test file and first test

---
+++ lib/shift.rb
@@ -0,0 +1,6 @@
+
+module Shift
+
+
+
+end
--- test/shift_test.rb
+++ test/shift_test.rb
@@ -3,15 +3,9 @@ require './test/test_helper'
 
 class ShiftTest < MiniTest::Test
 
-  def test_shift_exists
-    shift = Shift.new
-    assert_instance_of Shift, shift
+  def test_key_a_and_offset_a_sum_is_integer
+    enigma = Enigma.new
+    assert_instance_of Integer, engima.shift_a
   end
 
-  def test_shift_has_an_empty_hash
-    shift = Shift.new
-    expected = {}
-    assert_equal expected, shift.hash
-  end
-
-  
+end
