31aedcd004466f729da3e7ab9f2bc3a06d1c625d
Alek <alekaker@outlook.com>
2019-02-26 19:26:15 -0700

    adding shift method A and B

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,6 +1,14 @@
 
 module Shift
 
+  def shift_a
+    a_key + offset_a
+  end
+
+  def shift_b
+    b_key + offset_b
+  end
+
 
 
 end
--- test/shift_test.rb
+++ test/shift_test.rb
@@ -5,26 +5,23 @@ class ShiftTest < MiniTest::Test
 
   def test_key_a_and_offset_a_sum_is_integer
     enigma = Enigma.new
-    assert_instance_of Integer, engima.shift_a
+    assert_instance_of Integer, enigma.shift_a
   end
 
   def test_key_b_and_offset_b_sum_is_integer
     enigma = Enigma.new
-    assert_instance_of Integer, engima.shift_b
+    assert_instance_of Integer, enigma.shift_b
   end
 
   def test_key_c_and_offset_c_sum_is_integer
     enigma = Enigma.new
-    assert_instance_of Integer, engima.shift_c
+    assert_instance_of Integer, enigma.shift_c
   end
 
   def test_key_d_and_offset_d_sum_is_integer
     enigma = Enigma.new
-    assert_instance_of Integer, engima.shift_d
+    assert_instance_of Integer, enigma.shift_d
   end
-
-
-
 
 
 end
