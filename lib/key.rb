20d89d6f03349b4c8f76ba804b102f635db4f689
Alek <alekaker@outlook.com>
2019-02-26 17:46:00 -0700

    adding b key_test and method

--- lib/key.rb
+++ lib/key.rb
@@ -2,9 +2,15 @@
 module Key
 
   def random_number
-    rand(0..99999).to_s.rjust(5,'0')
+    @random_number ||= rand(0..99999).to_s.rjust(5,'0')
   end
 
   def a_key
+    random_number[0..1].to_i
+  end
+
+  def b_key
+    random_number[2..3].to_i
+  end
 
 end
--- test/key_test.rb
+++ test/key_test.rb
@@ -2,20 +2,29 @@ require './test/test_helper'
 
 class KeyTest < MiniTest::Test
 
-  def test_random_key_is_the_right_length
+  def test_random_number_is_the_right_length
     enigma = Enigma.new
-    assert_equal 5, enigma.random_key.length
+    assert_equal 5, enigma.random_number.length
   end
 
-  def test_random_key_is_the_right_data_type
+  def test_random_number_is_the_right_data_type
     enigma = Enigma.new
-    assert String, enigma.random_key
+    assert String, enigma.random_number
   end
 
   def test_A_key
     enigma = Enigma.new
-    key = enigma.random_key
-    assert_equal key(0..1), enigma.a_key
+    key = enigma.random_number
+    assert_equal key[0..1].to_i, enigma.a_key
   end
+
+  def test_B_key
+    enigma = Enigma.new
+    key = enigma.random_number
+    assert_equal key[2..3].to_i, enigma.b_key
+  end
+
+  
+
 
 end
