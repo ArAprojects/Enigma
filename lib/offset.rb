187c6b1ffe702940f6fafe0f591f51dac06fb9e4
Alek <alekaker@outlook.com>
2019-02-26 19:21:30 -0700

    small refactor of offset to work better with shift

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,23 +1,24 @@
 module Offset
 
   def todays_date_squared
-    todays_date.to_i**2
+    date = todays_date.to_i**2
+    date.to_s
   end
 
   def offset_a
-    todays_date_squared.to_s[-4]
+    todays_date_squared[-4].to_i
   end
 
   def offset_b
-    todays_date_squared.to_s[-3]
+    todays_date_squared[-3].to_i
   end
 
   def offset_c
-    todays_date_squared.to_s[-2]
+    todays_date_squared[-2].to_i
   end
 
   def offset_d
-    todays_date_squared.to_s[-1]
+    todays_date_squared[-1].to_i
   end
 
 end
