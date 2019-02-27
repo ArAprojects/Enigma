5204ccf7e367df479d574e5a4e2004c171212ff8
Alek <alekaker@outlook.com>
2019-02-26 18:30:21 -0700

    adding test and method to square todays date

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,6 +1,7 @@
 module Offset
 
 def todays_date_squared
+  todays_date.to_i**2
 end
 
 end
--- test/offset_test.rb
+++ test/offset_test.rb
@@ -8,4 +8,10 @@ class OffsetTest < MiniTest::Test
     assert_equal Date.today.strftime('%d%m%y'), enigma.todays_date
   end
 
+  def test_todays_date_can_be_squared
+    enigma = Enigma.new
+    Date.today.strftime('%d%m%y')
+    assert_equal Date.today.strftime('%d%m%y').to_i**2, enigma.todays_date_squared
+  end
+
 end
